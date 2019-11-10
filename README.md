009fb1e0a057d7a37ba17eea24a9380c2aebb3ca
Dan Lapp <dan.lapp@points.com>
2019-11-10 18:56:54 -0500

    Users table and add endpoint

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ Description of project
 ### Setting up virtualenv
   - install python3-venv
   - python3 -m venv venv
+  
+### Initialize test SQLite database
+  - run `python engine.py` to create database and tables
 
 ### Running dev server
   - activate venv with `source venv/bin/activate`
-  - export FLASK_APP=app.py
-  - flask run
+  - run `python app.py`
+  - load [0.0.0.0:6543](http://0.0.0.0:6543/)
--- engine.py
+++ engine.py
@@ -2,15 +2,22 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///tmp/test.db', echo=True)
+Base.metadata.create_all(engine)
 
 # https://docs.sqlalchemy.org/en/13/orm/tutorial.html
 class User(Base):
-    __tablename__ = 'users';
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
 
     def __repr__(self):
-        pass;
+        return '<User(first_name={self.first_name}, last_name={self.last_name})>'.format(self=self)
+
+    def __str__(self):
+        return 'User: {self.last_name}, {self.first_name}'.format(self=self)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
